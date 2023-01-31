0c9dc9d0447967b90fa9cdea4230337367e3af2a
SamueljWilson <115675537+SamueljWilson@users.noreply.github.com>
2023-01-30 19:31:00 -0800

    finished adding fine tuning support

--- src/main/java/frc/robot/subsystems/ArmInterp.java
+++ src/main/java/frc/robot/subsystems/ArmInterp.java
@@ -8,78 +8,80 @@ import java.util.Arrays;
 
 /** Add your docs here. */
 public class ArmInterp {
-  static class HtEntry{double height; double ticks; public HtEntry(double h, double t) {height = h; ticks = t;}}
+  static class HtEntry{double cycles; double height; public HtEntry(double c, double h) {cycles = c; height = h;}}
   static final HtEntry[] htTable = {
       new HtEntry(0, 0),
       new HtEntry(60, 10000)
   };
 
-  public static int heightIndex(double height) {
-    assert(height >= 0);
-    int index = Arrays.binarySearch(htTable, new HtEntry(height, 0), (a,b) -> Double.compare(a.height, b.height));
+  public static int cycleIndex(double cycles) {
+    assert(cycles >= 0);
+    int index = Arrays.binarySearch(htTable, new HtEntry(cycles, 0), (a,b) -> Double.compare(a.cycles, b.cycles));
     assert(index < htTable.length);
     return index;
   }
 
-  public static double heightToTicks(double height) {
-    int index = heightIndex(height);
+  public static double cyclesToHeight(double cycles) {
+    int index = cycleIndex(cycles);
      if (index == 0) {
-      return htTable[index].ticks;
-     }
-     else if (htTable[index].height == height) {
-       return htTable[index].ticks;
-     }
-     // Linear Interpolation
-     else {
-      double h0 = htTable[index-1].height;
-      double h1 = htTable[index].height;
-      double t0 = htTable[index-1].ticks;
-      double t1 = htTable[index].ticks;
-      double scaler = (height-h0) / (h1 - h0);
-      double ticksRange = t1 - t0;
-      return t0 + scaler*ticksRange;
-     }
-  }
-  
-  public static double ticksToHeight(double ticks) {
-    assert(ticks >= 0);
-    int index = Arrays.binarySearch(htTable, new HtEntry(0, ticks), (a,b) -> Double.compare(a.ticks, b.ticks));
-
-    if (index == 0) {
       return htTable[index].height;
      }
-     else if (index == htTable.length) {
-      return htTable[index-1].height;
-     }
-     else if (htTable[index].ticks == ticks) {
+     else if (htTable[index].cycles == cycles) {
        return htTable[index].height;
      }
      // Linear Interpolation
      else {
+      double c0 = htTable[index-1].cycles;
+      double c1 = htTable[index].cycles;
       double h0 = htTable[index-1].height;
       double h1 = htTable[index].height;
-      double t0 = htTable[index-1].ticks;
-      double t1 = htTable[index].ticks;
-      double scaler = (ticks-t0) / (t1 - t0);
+      double scaler = (cycles-c0) / (c1 - c0);
       double heightRange = h1 - h0;
       return h0 + scaler*heightRange;
      }
   }
 
-  public static double getTicksPerMeter(double height) {
+  public static int heightIndex(double height) {
+    assert(height >= 0);
+    int index = Arrays.binarySearch(htTable, new HtEntry(0, height), (a,b) -> Double.compare(a.height, b.height));
+    assert(index < htTable.length);
+    return index;
+  }
+  
+  public static double heightToCycles(double height) {
+    int index = heightIndex(height);
+    if (index == 0) {
+      return htTable[index].cycles;
+     }
+     else if (htTable[index].height == height) {
+       return htTable[index].cycles;
+     }
+     // Linear Interpolation
+     else {
+      double c0 = htTable[index-1].cycles;
+      double c1 = htTable[index].cycles;
+      double h0 = htTable[index-1].height;
+      double h1 = htTable[index].height;
+      double scaler = (height-h0) / (h1 - h0);
+      double cycleRange = c1 - c0;
+      return c0 + scaler*cycleRange;
+     }
+  }
+
+  public static double getCyclesPerMeter(double height) {
     if (height == 0.0) {
       return 0.0;
     }
     int indexGE = heightIndex(height);
     assert(indexGE > 0);
     int indexLT = indexGE - 1;
+    double deltaC = htTable[indexGE].cycles - htTable[indexLT].cycles;
     double deltaH = htTable[indexGE].height - htTable[indexLT].height;
-    double deltaT = htTable[indexGE].ticks - htTable[indexLT].ticks;
-    return deltaT / deltaH;
+    return deltaC / deltaH;
   }
 
-  public static double vheightToTPM(double metersPerSecond, double height) {
-    double slope = getTicksPerMeter(height);
+  public static double vheightToRPM(double metersPerSecond, double height) {
+    double slope = getCyclesPerMeter(height);
     double secondsPerMinute = 60;
     return slope * metersPerSecond * secondsPerMinute;
   }
