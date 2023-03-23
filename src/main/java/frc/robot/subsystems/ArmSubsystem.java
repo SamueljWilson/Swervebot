dde0a956a79a1cd376cb1c57e3082bcbf377b98a
SamueljWilson <115675537+SamueljWilson@users.noreply.github.com>
2023-03-22 18:25:31 -0700

    Refactored the arm subsystem to use the wrist subsystem

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -9,16 +9,17 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.SparkMaxLimitSwitch;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ArmConstants;
+import frc.robot.Constants.WristConstants;
 
 public class ArmSubsystem extends SubsystemBase {
   CANSparkMax m_armMotor = new CANSparkMax(ArmConstants.kArmMotorPort, MotorType.kBrushless);
   SparkMaxLimitSwitch m_forwardLimitSwitch = m_armMotor.getForwardLimitSwitch(SparkMaxLimitSwitch.Type.kNormallyOpen);
   SparkMaxLimitSwitch m_reverseLimitSwitch = m_armMotor.getReverseLimitSwitch(SparkMaxLimitSwitch.Type.kNormallyOpen);
+  WristSubsystem m_wrist;
 
   public enum InitState {
     UNINITIALIZED,
@@ -26,25 +27,21 @@ public class ArmSubsystem extends SubsystemBase {
   }
 
   InitState m_initState = InitState.UNINITIALIZED;
-    
-  DoubleSolenoid m_wristPiston = ArmConstants.kArmStubOut ? null : new DoubleSolenoid(
-    PneumaticsModuleType.REVPH,
-    ArmConstants.kWristSolenoidForwardChannel,
-    ArmConstants.kWristSolenoidBackwardChannel);
 
   /** Creates a new ArmSubsystem. */
-  public ArmSubsystem() {}
+  public ArmSubsystem(WristSubsystem wrist) {
+    m_wrist = wrist;
+  }
 
   private DoubleSolenoid.Value getWristPosition() {
-    return m_wristPiston.get();
+    return m_wrist.getWristPosition();
   }
 
   public Command moveHome() {
     return runOnce(
       () -> {
-        DoubleSolenoid.Value wristPosition = getWristPosition();
-        if (wristPosition == ArmConstants.kWristExtended) {
-          m_wristPiston.set(ArmConstants.kWristRetracted);
+        if (m_wrist.getWristPosition() == WristConstants.kWristExtended) {
+          m_wrist.retractWrist();
         }
         m_armMotor.getPIDController().setReference(ArmInterp.cyclesToHeight(ArmConstants.kHomeHeight),
         ControlType.kPosition);
@@ -119,11 +116,11 @@ public class ArmSubsystem extends SubsystemBase {
     DoubleSolenoid.Value wristPosition = getWristPosition();
     double armPosition = getCycles();
     assert(ArmConstants.kWristRetractionCycles < ArmConstants.kWristExtensionCycles);
-    if (wristPosition == ArmConstants.kWristExtended && armPosition <= ArmConstants.kWristRetractionCycles) {
-      m_wristPiston.set(ArmConstants.kWristRetracted);
+    if (wristPosition == WristConstants.kWristExtended && armPosition <= ArmConstants.kWristRetractionCycles) {
+      m_wrist.retractWrist();
     }
-    else if (wristPosition == ArmConstants.kWristRetracted && armPosition >= ArmConstants.kWristExtensionCycles) {
-      m_wristPiston.set(ArmConstants.kWristExtended);
+    else if (wristPosition == WristConstants.kWristRetracted && armPosition >= ArmConstants.kWristExtensionCycles) {
+      m_wrist.extendWrist();
     }
   }
 }
