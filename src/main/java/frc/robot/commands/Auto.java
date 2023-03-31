b029e194231bbdfed49662dac8ea457739aed3aa
SamueljWilson <115675537+SamueljWilson@users.noreply.github.com>
2023-03-30 18:05:05 -0700

    Changed the robot relative distance from 2 meters to 0.5 meters

--- src/main/java/frc/robot/commands/Auto.java
+++ src/main/java/frc/robot/commands/Auto.java
@@ -268,7 +268,7 @@ public class Auto {
       DriveTrajectory.generateTrajectory(
         mirror.apply(new Pose2d(0, 0, new Rotation2d(Math.PI))),
         List.of(),
-        mirror.apply(new Pose2d(2.0, 0, new Rotation2d(Math.PI)))); //TOOD: CHANGE BACK TO 0.5
+        mirror.apply(new Pose2d(0.5, 0, new Rotation2d(Math.PI))));
   }
 
   private static Trajectory hTrajectoryPlaceCross0(MirrorInterface mirror) {
