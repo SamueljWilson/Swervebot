fe6d7b1ce96202f3b9446f9f34cbc54155fd58df
SamueljWilson <samspamgbot@gmail.com>
2023-12-18 17:53:48 -0800

    WPILib 2024 replaced CommandBase with CommandWPILib 2024 replaced CommandBase with Command

--- src/main/java/frc/robot/commands/MoveArmSync.java
+++ src/main/java/frc/robot/commands/MoveArmSync.java
@@ -5,7 +5,6 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.ArmConstants;
 import frc.robot.subsystems.ArmInterp;
 import frc.robot.subsystems.ArmSubsystem;
