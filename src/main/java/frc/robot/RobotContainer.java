b45a95455c2e9d086eafb56651152c4cf53c79c8
teravik3 <teravik3@gmail.com>
2024-01-27 17:07:05 -0800

    Adding Pathplanner Commands
    These commits add the three note stage auto pathplanner autos and paths to Thor_2023.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import com.pathplanner.lib.auto.NamedCommands;
 import com.pathplanner.lib.commands.PathPlannerAuto;
 
 import edu.wpi.first.math.MathUtil;
@@ -15,6 +16,8 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.Constants.OIConstants;
+import frc.robot.commands.PickupCommand;
+import frc.robot.commands.ShooterCommand;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.Limelight;
 import frc.robot.commands.TargetNote;
@@ -57,6 +60,9 @@ public class RobotContainer {
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+
+    NamedCommands.registerCommand("ShooterCommand", new ShooterCommand());
+    NamedCommands.registerCommand("PickupCommand", new PickupCommand());
     // Configure the button bindings
     configureButtonBindings();
     configureAutoRoutines();
