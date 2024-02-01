1f16f3de6e3fc909bb1e9c4ff0ce8dab6699d670
SamueljWilson <samspamgbot@gmail.com>
2024-01-31 17:40:45 -0800

    Renamed the camera subsystem to m_cameraSystem and removed  unnecessary dependency

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -32,8 +32,8 @@ public class RobotContainer {
   public final DriveSubsystem m_robotDrive = new DriveSubsystem();
   public final Limelight m_limelight = new Limelight();
   private final SendableChooser<Command> m_chooser = new SendableChooser<>();
-  private final CameraSubsystem m_cameras = new CameraSubsystem(PhotonVisionConstants.kCameraName1, PhotonVisionConstants.kCameraName2);
-  public final DriveSubsystem m_robotDrive = new DriveSubsystem(m_cameras);
+  private final CameraSubsystem m_cameraSystem = new CameraSubsystem(PhotonVisionConstants.kCameraName1, PhotonVisionConstants.kCameraName2);
+  public final DriveSubsystem m_robotDrive = new DriveSubsystem(m_cameraSystem);
 
   // The driver's controller
   GenericHID m_driverController = new GenericHID(OIConstants.kDriverControllerPort);
@@ -78,7 +78,7 @@ public class RobotContainer {
               reverseFactor*joystickTransform(m_driverController.getRawAxis(OIConstants.kLeftJoyXAxis))*OIConstants.kMaxMetersPerSec,
               -joystickTransform(m_driverController.getRawAxis(OIConstants.kRightJoyXAxis))*OIConstants.kMaxRadPerSec,
               true);
-          }, m_robotDrive, m_cameras
+          }, m_robotDrive
         )
       );
 
