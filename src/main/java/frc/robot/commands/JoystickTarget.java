d5f44f9f489204c2a6504a02aca74e7d09c3d4ab
Keira P <keira@JASON-WIN8.nuggetmarket.com>
2024-02-01 19:18:46 -0800

    made updatable constants

--- src/main/java/frc/robot/commands/JoystickTarget.java
+++ src/main/java/frc/robot/commands/JoystickTarget.java
@@ -5,6 +5,7 @@ import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.OIConstants;
 import frc.robot.Constants.SwerveModuleConstants;
@@ -35,10 +36,16 @@ public class JoystickTarget extends Command {
   public void initialize() {
     thetaController.reset(0);
     thetaController.setTolerance(VisionConstants.kTargetingTolerance);
+    SmartDashboard.putNumber("Max Angular Speed", SmartDashboard.getNumber("Max Angular Speed", SwerveModuleConstants.kMaxAngularSpeedRadiansPerSecond));
+    SmartDashboard.putNumber("Max Angular Acceleration", SmartDashboard.getNumber("Max Angular Acceleration", SwerveModuleConstants.kMaxAngularAccelerationRadiansPerSecondSquared));
+    SmartDashboard.putNumber("kP", SmartDashboard.getNumber("kP", SwerveModuleConstants.kPTurningController));
+    SmartDashboard.putNumber("kI", SmartDashboard.getNumber("kI", SwerveModuleConstants.kITurningController));
+    SmartDashboard.putNumber("kD", SmartDashboard.getNumber("kD", SwerveModuleConstants.kDTurningController));
   }
 
   @Override
   public void execute() {
+    updateConstants();
     double x = m_limelight.getX();
     double xVelocity = thetaController.calculate(Units.degreesToRadians(x));
     double reverseFactor = -1.0;
@@ -55,5 +62,24 @@ public class JoystickTarget extends Command {
     double postDeadbandValue = MathUtil.applyDeadband(value, OIConstants.kJoystickDeadband);
     double postDeadbandValueSquared = postDeadbandValue * Math.abs(postDeadbandValue);
     return postDeadbandValueSquared * speedCoef;
+  }
+
+  private void updateConstants() {
+    double maxAngularSpeed = SmartDashboard.getNumber("Max Angular Speed", SwerveModuleConstants.kMaxAngularSpeedRadiansPerSecond);
+    double maxAngularAcceleration = SmartDashboard.getNumber("Max Angular Acceleration", SwerveModuleConstants.kMaxAngularAccelerationRadiansPerSecondSquared);
+    double currentMaxAngularSpeed = thetaController.getConstraints().maxVelocity;
+    double currentMaxAngularAcceleration = thetaController.getConstraints().maxAcceleration;
+    if (currentMaxAngularSpeed != maxAngularSpeed || currentMaxAngularAcceleration != maxAngularAcceleration) {
+      System.out.println("updating constraints");
+      thetaController.setConstraints(new TrapezoidProfile.Constraints(maxAngularSpeed, maxAngularAcceleration));
+    }
+
+    double p = SmartDashboard.getNumber("kP", SwerveModuleConstants.kPTurningController);
+    double i = SmartDashboard.getNumber("kI", SwerveModuleConstants.kITurningController);
+    double d = SmartDashboard.getNumber("kD", SwerveModuleConstants.kDTurningController);
+    if (thetaController.getP() != p || thetaController.getI() != i || thetaController.getD() != d) {
+      System.out.println("updating pid");
+      thetaController.setPID(p, i, d);
+    }
   }
 }
