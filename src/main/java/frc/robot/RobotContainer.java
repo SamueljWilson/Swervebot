b9e00db5a538d33afb483b0b233168b565e16d4e
Jason Evans <jasone@canonware.com>
2024-02-24 18:30:04 -0800

    Fix joystick-to-(x,y) conversion

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -42,12 +42,35 @@ public class RobotContainer {
 
   GenericHID m_driverController = new GenericHID(OIConstants.kDriverControllerPort);
 
-  double reverseFactor = DriverStation.getAlliance().get() == Alliance.Blue ? 1 : -1;
+  double m_reverseFactor = DriverStation.getAlliance().get() == Alliance.Blue ? 1 : -1;
 
   private static double joystickTransform(double value) {
     double postDeadbandValue = MathUtil.applyDeadband(value, OIConstants.kJoystickDeadband);
     double postDeadbandValueSquared = postDeadbandValue * Math.abs(postDeadbandValue);
     return postDeadbandValueSquared;
+  }
+
+  private double getXSpeedInput() {
+    // The Y axis on the controller is inverted! Pushing forward (up) generates a negative value.
+    // Negate the input value here.
+    return m_reverseFactor
+      * joystickTransform(-m_driverController.getRawAxis(OIConstants.kLeftJoyYAxis))
+      * OIConstants.kMaxMetersPerSec;
+  }
+
+  private double getYSpeedInput() {
+    // The X axis on the controller behaves as expected (right is positive), but we're using it for
+    // Y axis control, where left is positive. Negate the input value here.
+    return m_reverseFactor
+      * joystickTransform(-m_driverController.getRawAxis(OIConstants.kLeftJoyXAxis))
+      * OIConstants.kMaxMetersPerSec;
+  }
+
+  private double getRotationSpeedInput() {
+    // Moving the joystick to the right causes positive input, which we negate in order to rotate
+    // clockwise.
+    return -joystickTransform(m_driverController.getRawAxis(OIConstants.kRightJoyXAxis))
+      * OIConstants.kMaxRadPerSec;
   }
 
   public RobotContainer() {
@@ -61,9 +84,9 @@ public class RobotContainer {
         new RunCommand(
           () -> {
             m_robotDrive.drive(
-              reverseFactor*joystickTransform(m_driverController.getRawAxis(OIConstants.kLeftJoyYAxis))*OIConstants.kMaxMetersPerSec,
-              reverseFactor*joystickTransform(m_driverController.getRawAxis(OIConstants.kLeftJoyXAxis))*OIConstants.kMaxMetersPerSec,
-              -joystickTransform(m_driverController.getRawAxis(OIConstants.kRightJoyXAxis))*OIConstants.kMaxRadPerSec,
+              getXSpeedInput(),
+              getYSpeedInput(),
+              getRotationSpeedInput(),
               true);
           }, m_robotDrive
         )
@@ -86,8 +109,8 @@ public class RobotContainer {
       .whileTrue(new JoystickTargetNote(
         m_robotDrive,
         m_limelight,
-        () -> reverseFactor * joystickTransform(m_driverController.getRawAxis(OIConstants.kLeftJoyYAxis)) * OIConstants.kMaxMetersPerSec,
-        () -> reverseFactor * joystickTransform(m_driverController.getRawAxis(OIConstants.kLeftJoyXAxis)) * OIConstants.kMaxMetersPerSec
+        () -> getXSpeedInput(),
+        () -> getYSpeedInput()
       ));
   }
 
